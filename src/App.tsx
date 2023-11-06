e1b200e4ed20e724f2f052ff8518cabc8cd9cb0a
hamidie00235 <ahmathisseinh@gmail.com>
2023-11-06 20:53:10 +0300

    Home Page - Library Services Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/carousel';
 import { Heros } from './layouts/HomePage/Heros';
+import { LibraryServices } from './layouts/HomePage/LibraryServices';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <ExploreTopBooks/>
       <Carousel/>
       <Heros/>
+      <LibraryServices/>
     </div>
   );
 }
