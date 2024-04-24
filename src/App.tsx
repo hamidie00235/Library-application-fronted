856915d09ce2b476df2708c1d54af33f5ff5ed45
hamidie00235 <ahmathisseinh@gmail.com>
2024-04-25 01:00:04 +0300

    section 27:library project-librarty services

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
-// import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
+import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
 // import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
@@ -57,8 +57,8 @@ export const App = () => {
           />
           <Route path='/login/callback' component={LoginCallback} />
           <SecureRoute path='/shelf'> <ShelfPage/> </SecureRoute>
-          {/* <SecureRoute path='/messages'> <MessagesPage/> </SecureRoute>
-          <SecureRoute path='/admin'> <ManageLibraryPage/> </SecureRoute> */}
+          <SecureRoute path='/messages'> <MessagesPage/> </SecureRoute>
+          {/* <SecureRoute path='/admin'> <ManageLibraryPage/> </SecureRoute> */}
         </Switch>
       </div>
       <Footer />
