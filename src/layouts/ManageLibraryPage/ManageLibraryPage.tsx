af72e79ec3c52fc3fe5b3c19da96b609074d360d
hamidie00235 <ahmathisseinh@gmail.com>
2024-05-08 01:18:32 +0300

    React - Change Quantity of Book HTML/CSS

--- src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { AddNewBook } from './components/AddNewBook';
 import { AdminMessages } from './components/AdminMessages';
-// import { ChangeQuantityOfBooks } from './components/ChangeQuantityOfBooks';
+import { ChangeQuantityOfBooks } from './components/ChangeQuantityOfBooks';
+
 
 export const ManageLibraryPage = () => {
 
@@ -63,7 +64,7 @@ export const ManageLibraryPage = () => {
                             <AddNewBook/>
                     </div>
                     <div className='tab-pane fade' id='nav-quantity' role='tabpanel' aria-labelledby='nav-quantity-tab'>
-                       {/* {changeQuantityOfBooksClick ? <ChangeQuantityOfBooks/> : <></>} */}
+                       {changeQuantityOfBooksClick ? <ChangeQuantityOfBooks/> : <></>}
                     </div>
                     <div className='tab-pane fade' id='nav-messages' role='tabpanel' aria-labelledby='nav-messages-tab'>
                         {messagesClick ? <AdminMessages/> : <></>}
